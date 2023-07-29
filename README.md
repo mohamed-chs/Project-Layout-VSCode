39381e9edc5ad1bd2df6f9902d7bbe1349c4da61
Mohamed Cheikh Sidiya <mohamedcheikhsidiya77@gmail.com>
2023-07-29 11:06:43 +0000

    second  commit

--- README.md
+++ README.md
@@ -1,65 +1,108 @@
-# Project-Layout-VSCode README
+# Project Layout
 
-This is the README for your extension "Project-Layout-VSCode". After writing up a brief description, we recommend including the following sections.
+Project Layout is a powerful extension that helps you visualize the organization of your codebase within VSCode. It creates a text-based project structure, which makes it easier to understand your project's layout and file dependencies. You can choose to export the structure in different formats: Markdown, JSON, or a simple text-based tree structure.
+
+This extension is a great tool for developers, especially for large projects, or when joining a new project and trying to understand the project structure quickly.
 
 ## Features
 
-Describe specific features of your extension including screenshots of your extension in action. Image paths are relative to this README file.
+### Generate Project Layout
+With a simple command, the extension will scan your project and generate a detailed representation of its structure, including folders and files. The tool smartly avoids directories and file types you don't want to include, based on your settings.
 
-For example if there is an image subfolder under your extension project workspace:
+### Configurable Output
+You have the flexibility to choose the format of your project's structure. The options include Markdown for those who prefer a more styled and structured format, JSON for those who want a format that can easily be read programmatically, and plain text for a quick and clean overview.
 
-\!\[feature X\]\(images/feature-x.png\)
+### Configurable Ignore Settings
+You can specify the directories and file types to be ignored when generating the project structure. This feature is useful for excluding directories like `node_modules` or ignoring file types such as binary or image files. Additionally, you can specify a text file containing a list of directories to be ignored. Each line should represent a path to a directory to be ignored.
 
-> Tip: Many popular extensions utilize animations. This is an excellent way to show off your extension! We recommend short, focused animations that are easy to follow.
+### Output Location
+Customize the location and name of the output file to match your preferences. By default, the output file will be saved in your project's root directory, but you can specify any path you want.
 
-## Requirements
+## Usage
 
-If you have any requirements or dependencies, add a section describing those and how to install and configure them.
+### To generate your project structure:
+1. Open the command palette with `Ctrl+Shift+P` (or `Cmd+Shift+P` on macOS).
+2. Type "Generate Project Structure" and hit `Enter`.
 
-## Extension Settings
+Now, a file representing your project structure will be generated at your specified location!
 
-Include if your extension adds any VS Code settings through the `contributes.configuration` extension point.
+### To customize the extension settings:
+Go to `File > Preferences > Settings` and search for "Project Layout". Here, you'll find options to tailor the extension to your preferences:
 
-For example:
+- **Output Format**: Choose between `"markdown"`, `"json"`, or `"text"`. This will determine how your project structure is formatted.
 
-This extension contributes the following settings:
+- **Output File Name**: Specify the name for the output file. By default, the file is named `"project_structure.md"`, `"project_structure.json"` or `"project_structure.txt"` depending on your chosen format.
 
-* `myExtension.enable`: Enable/disable this extension.
-* `myExtension.thing`: Set to `blah` to do something.
+- **Output File Location**: Choose where the output file should be saved. The default is `.` (the root of your project).
 
-## Known Issues
+- **Directories to Ignore**: A list of directories to exclude from the project structure. Directories should be specified as relative paths from the root of your workspace.
 
-Calling out known issues can help limit users opening duplicate issues against your extension.
+- **File Extensions to Ignore**: A list of file extensions to exclude. Each item should start with a period (e.g. `".exe"`, `".tmp"`).
+
+- **Directories to Ignore File Path**: The path to a text file with a list of directories to ignore. Each line in the file should contain one directory path. The default value is `"./.gitignore"`.
+
+## Example
+If we have the following project structure:
+
+```
+/my_project
+├── /src
+│   ├── script.js
+│   └── style.css
+├── /test
+│   └── test_script.js
+├── .gitignore
+└── README.md
+```
+
+The extension might generate the following output in Markdown:
+
+```md
+# My Project
+
+- **/src**
+  - script.js
+  - style.css
+- **/test**
+  - test_script.js
+- .gitignore
+- README.md
+```
+
+Or the following in JSON:
+
+```json
+{
+  "My Project": {
+    "src": [
+      "script.js",
+      "style.css"
+    ],
+    "test": [
+      "test_script.js"
+    ],
+    "files": [
+      ".gitignore",
+      "README.md"
+    ]
+  }
+}
+```
+
+## Installation
+
+You can install the Project Layout extension directly from the Visual Studio Code Marketplace. Just search for "Project Layout" and click on "Install".
 
 ## Release Notes
 
-Users appreciate release notes as you update your extension.
-
 ### 1.0.0
 
-Initial release of ...
+Initial release.
 
-### 1.0.1
+## Contributions
 
-Fixed issue #.
+Your contributions are always welcome! Feel free to create issues or submit pull requests.
 
-### 1.1.0
+## License
 
-Added features X, Y, and Z.
-
----
-
-## Working with Markdown
-
-You can author your README using Visual Studio Code.  Here are some useful editor keyboard shortcuts:
-
-* Split the editor (`Cmd+\` on macOS or `Ctrl+\` on Windows and Linux)
-* Toggle preview (`Shift+Cmd+V` on macOS or `Shift+Ctrl+V` on Windows and Linux)
-* Press `Ctrl+Space` (Windows, Linux, macOS) to see a list of Markdown snippets
-
-## For more information
-
-* [Visual Studio Code's Markdown Support](http://code.visualstudio.com/docs/languages/markdown)
-* [Markdown Syntax Reference](https://help.github.com/articles/markdown-basics/)
-
-**Enjoy!**
+This project is licensed under the terms of the MIT license.
--- extension.js
+++ extension.js
@@ -1,36 +1,235 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
-
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+// Importing necessary modules
+const vscode = require("vscode");
+const fs = require("fs");
+const path = require("path");
 
 /**
  * @param {vscode.ExtensionContext} context
  */
+// Activating the extension
 function activate(context) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "Project-Layout-VSCode" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('Project-Layout-VSCode.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
+  // Registering the command and pushing it to the context subscriptions
+  let disposable = vscode.commands.registerCommand(
+    "extension.generateProjectStructure",
+    generateProjectStructure
+  );
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Project Layout!');
-	});
-
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
-function deactivate() {}
+// Function to generate the project structure
+function generateProjectStructure() {
+  // Getting the root path
+  const rootPath = vscode.workspace.workspaceFolders
+    ? vscode.workspace.workspaceFolders[0].uri.fsPath
+    : undefined;
 
+  if (!rootPath) {
+    vscode.window.showErrorMessage("No workspace folder is open");
+    return;
+  }
+
+  // Getting the settings
+  const config = vscode.workspace.getConfiguration('vscodeProjectStructure');
+  const outputFileName = config.get('outputFileName');
+  const outputFileLocation = config.get('outputFileLocation');
+  const additionalIgnoreDirs = config.get('additionalIgnoreDirs');
+  const ignoreExtensions = config.get('ignoreExtensions');
+  const outputFormat = config.get('outputFormat');
+
+  // Getting the directories to ignore
+  let ignoreDirs = getDirectoriesToIgnore();
+  ignoreDirs.push(...additionalIgnoreDirs);
+
+  // Starting the output string
+  let output = "";
+  switch (outputFormat) {
+    case 'markdown':
+      output += "--- Folder Structure ---\n";
+      output += getFolderStructureMarkdown(rootPath, ignoreDirs, ignoreExtensions, 0);
+      break;
+    case 'json':
+      output += JSON.stringify(getFolderStructureJson(rootPath, ignoreDirs, ignoreExtensions), null, 2);
+      break;
+    default:
+      output += "--- Folder Structure ---\n";
+      output += getFolderStructurePlain(rootPath, ignoreDirs, ignoreExtensions, 0);
+  }
+
+  // Determine the file extension based on the output format
+  let fileExtension;
+  switch (outputFormat) {
+    case 'json':
+      fileExtension = '.json';
+      break;
+    case 'markdown':
+      fileExtension = '.md';
+      break;
+    default:
+      fileExtension = '.txt';
+  }
+
+  // Add the extension to the output file name
+  const completeOutputFileName = `${outputFileName}${fileExtension}`;
+
+  // Generating the output path
+  const outputPath = path.join(rootPath, outputFileLocation, completeOutputFileName);
+
+  // Writing the output to the file
+  fs.writeFileSync(outputPath, output);
+
+  // Showing a message that the project structure was generated successfully
+  vscode.window.showInformationMessage("Project structure generated successfully");
+}
+
+
+// Functions to get the folder structure
+// Function to get the folder structure in JSON format
+function getFolderStructureJson(dir, ignoreDirs, ignoreExtensions) {
+  let structure = {};
+
+  // If the directory exists
+  if (fs.existsSync(dir)) {
+    // Reading the files in the directory
+    const files = fs.readdirSync(dir);
+    files.forEach((file) => {
+      const fullPath = path.join(dir, file);
+      const relativePath = path.relative(process.cwd(), fullPath);
+
+      // If the file is a directory
+      if (fs.lstatSync(fullPath).isDirectory()) {
+        if (shouldIgnore(relativePath, ignoreDirs, ignoreExtensions, true)) {
+          structure[file] = "(...)";
+        } else {
+          structure[file] = getFolderStructureJson(fullPath, ignoreDirs, ignoreExtensions);
+        }
+      } else {
+        // If the file is not a directory and should not be ignored
+        if (!shouldIgnore(relativePath, ignoreDirs, ignoreExtensions)) {
+          structure[file] = null;
+        }
+      }
+    });
+  }
+
+  return structure;
+}
+
+
+// Function to get the folder structure in Markdown format
+function getFolderStructureMarkdown(dir, ignoreDirs, ignoreExtensions, level) {
+  let output = "";
+
+  // If the directory exists
+  if (fs.existsSync(dir)) {
+    // Reading the files in the directory
+    const files = fs.readdirSync(dir);
+    files.forEach((file) => {
+      const fullPath = path.join(dir, file);
+      const relativePath = path.relative(process.cwd(), fullPath);
+      const indent = " ".repeat(level * 2);
+
+      if (fs.lstatSync(fullPath).isDirectory()) {
+        if (shouldIgnore(relativePath, ignoreDirs, ignoreExtensions, true)) {
+          output += `${indent}- ${file}/ (...)\n`;
+        } else {
+          output += `${indent}- ${file}/\n`;
+          output += getFolderStructureMarkdown(fullPath, ignoreDirs, ignoreExtensions, level + 1);
+        }
+      } else if (!shouldIgnore(relativePath, ignoreDirs, ignoreExtensions)) {
+        output += `${indent}- ${file}\n`;
+      }
+    });
+  }
+
+  return output;
+}
+
+
+// Function to get the folder structure in plain format
+function getFolderStructurePlain(dir, ignoreDirs, ignoreExtensions, level) {
+  let output = "";
+
+  // If the directory exists
+  if (fs.existsSync(dir)) {
+    // Reading the files in the directory
+    const files = fs.readdirSync(dir);
+    files.forEach((file, index) => {
+      const fullPath = path.join(dir, file);
+      const relativePath = path.relative(process.cwd(), fullPath);
+      const isLastFile = index === files.length - 1;
+      const prefix = level === 0 ? "" : isLastFile ? "└── " : "├── ";
+      const indent = " ".repeat(level * 4) + (level > 0 ? prefix : "");
+
+      if (fs.lstatSync(fullPath).isDirectory()) {
+        if (shouldIgnore(relativePath, ignoreDirs, ignoreExtensions, true)) {
+          output += indent + `[${file}] (...)\n`;
+        } else {
+          output += indent + `[${file}]\n`;
+          output += getFolderStructurePlain(fullPath, ignoreDirs, ignoreExtensions, level + 1);
+        }
+      } else if (!shouldIgnore(relativePath, ignoreDirs, ignoreExtensions)) {
+        output += indent + file + "\n";
+      }
+    });
+  }
+
+  return output;
+}
+
+
+
+// Function to get the directories to ignore
+function getDirectoriesToIgnore() {
+  // Getting the root path
+  const rootPath = vscode.workspace.workspaceFolders
+    ? vscode.workspace.workspaceFolders[0].uri.fsPath
+    : undefined;
+
+  // If there's no workspace, return an empty array
+  if (!rootPath) {
+    return [];
+  }
+
+  // Getting the settings
+  const config = vscode.workspace.getConfiguration('vscodeProjectStructure');
+  const relativePathToIgnore = config.get('ignoreDirPath');
+
+  // Resolve the absolute path to the ignore file
+  const absolutePathToIgnore = path.resolve(rootPath, relativePathToIgnore);
+
+  if (!fs.existsSync(absolutePathToIgnore)) {
+    return [];
+  }
+
+  // Reading the contents of the file and splitting it by lines
+  let contents = fs.readFileSync(absolutePathToIgnore, 'utf8');
+  let dirs = contents.split('\n').map(line => line.trim()).filter(line => line !== '');
+
+  return dirs;
+}
+
+
+
+// Function to determine if a file should be ignored
+function shouldIgnore(relativePath, ignoreDirs, ignoreExtensions, isDirectory = false) {
+  if (isDirectory) {
+    return ignoreDirs.some(dir => relativePath.includes(dir));
+  }
+  const fileExtension = path.extname(relativePath);
+  return ignoreExtensions.includes(fileExtension);
+}
+
+
+// Deactivating the extension
+function deactivate() { }
+
+// Exporting the activate and deactivate functions
 module.exports = {
-	activate,
-	deactivate
-}
+  activate,
+  deactivate,
+};
